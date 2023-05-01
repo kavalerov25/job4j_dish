a7e2fc7188105e036b90080d428af948f6e00468
kavalerov25 <paradise_777@mail.ru>
2023-05-01 14:57:32 +0300

    Подготовка проекта

---
+++ src/test/java/ru/job4j/dish/DishApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.dish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
